3892db0d29759747d58dee9ea5ac803253e61f26
kepope3 <priest3210@gmail.com>
2023-08-02 11:06:32 +0100

    added loop

--- detectpieces.py
+++ detectpieces.py
@@ -4,83 +4,85 @@ import picamera
 import time
 
 camera = picamera.PiCamera()
-
 camera.resolution = (640, 480)
-camera.start_preview()
-time.sleep(20)
-camera.stop_preview()
 camera.rotation = 0
-    
-    #image = np.empty((camera.resolution[1], camera.resolution[0], 3), dtype=np.uint8)
-camera.capture("chessimage.jpg")
-    
-image = cv2.imread("chessimage.jpg")
 
 # Load calibration data
 with np.load('./camcalib/calibration_data_npz.npz') as data:
     K = data['K']
     D = data['D']
 
-h, w = image.shape[:2]
-map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, (w,h), cv2.CV_16SC2)
-undistorted_img = cv2.remap(image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+while True:
+    camera.start_preview()
+    time.sleep(2)
+    camera.stop_preview()
+    camera.capture("chessimage.jpg")
+    
+    image = cv2.imread("chessimage.jpg")
 
-# Specify the crop dimensions (x, y, w, h)
-crop_rectangle = (70, 10, 450, 450)
-x, y, w, h = crop_rectangle
+    h, w = image.shape[:2]
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, (w,h), cv2.CV_16SC2)
+    undistorted_img = cv2.remap(image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
-# Crop the image
-cropped_image = undistorted_img[y:y+h, x:x+w]
+    # Specify the crop dimensions (x, y, w, h)
+    crop_rectangle = (70, 10, 450, 450)
+    x, y, w, h = crop_rectangle
 
-# Convert to HSV for better color isolation
-hsv_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2HSV)
+    # Crop the image
+    cropped_image = undistorted_img[y:y+h, x:x+w]
 
-# Define color range for red pieces (remember HSV color space is different!)
-lower_red1 = np.array([0, 50, 50])
-upper_red1 = np.array([10, 255, 255])
-lower_red2 = np.array([160, 50, 50])
-upper_red2 = np.array([180, 255, 255])
+    # Convert to HSV for better color isolation
+    hsv_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2HSV)
 
-# Threshold the HSV image to get only red colors
-red_mask1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
-red_mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+    # Define color range for red pieces (remember HSV color space is different!)
+    lower_red1 = np.array([0, 50, 50])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([160, 50, 50])
+    upper_red2 = np.array([180, 255, 255])
 
-# Combine the two masks
-red_mask = cv2.bitwise_or(red_mask1, red_mask2)
-# Define the grid dimensions
-grid_size = 8
+    # Threshold the HSV image to get only red colors
+    red_mask1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+    red_mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
 
-# Calculate the step size
-step_size_x = w // grid_size
-step_size_y = h // grid_size
+    # Combine the two masks
+    red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+    # Define the grid dimensions
+    grid_size = 8
 
-# Draw the grid and check for red pieces
-for i in range(0, w, step_size_x):
-    for j in range(0, h, step_size_y):
-        # Extract the cell
-        cell = red_mask[j:j+step_size_y, i:i+step_size_x]
+    # Calculate the step size
+    step_size_x = w // grid_size
+    step_size_y = h // grid_size
 
-        # Calculate the proportion of red pixels
-        red_pixels = np.sum(cell == 255)
-        total_pixels = np.size(cell)
-        if red_pixels / total_pixels > 0.020:  # Modify this threshold based on your specific needs
-            cv2.rectangle(cropped_image, (i, j), (i+step_size_x, j+step_size_y), (0, 0, 255), 2)
+    # Draw the grid and check for red pieces
+    for i in range(0, w, step_size_x):
+        for j in range(0, h, step_size_y):
+            # Extract the cell
+            cell = red_mask[j:j+step_size_y, i:i+step_size_x]
 
-    cv2.line(red_mask, (i, 0), (i, h), (0, 255, 0), 1)
+            # Calculate the proportion of red pixels
+            red_pixels = np.sum(cell == 255)
+            total_pixels = np.size(cell)
+            if red_pixels / total_pixels > 0.020:  # Modify this threshold based on your specific needs
+                cv2.rectangle(cropped_image, (i, j), (i+step_size_x, j+step_size_y), (0, 0, 255), 2)
 
-for i in range(0, h, step_size_y):
-    cv2.line(red_mask, (0, i), (w, i), (0, 255, 0), 1)
+        cv2.line(red_mask, (i, 0), (i, h), (0, 255, 0), 1)
 
-#display red mask image
-cv2.imshow("Red mask", red_mask)
-cv2.waitKey(0)
+    for i in range(0, h, step_size_y):
+        cv2.line(red_mask, (0, i), (w, i), (0, 255, 0), 1)
 
-# Display the cropped image
-cv2.imshow('Cropped and Gridded Image', cropped_image)
-cv2.waitKey(0)
+    #display red mask image
+    cv2.imshow("Red mask", red_mask)
+    cv2.waitKey(1)
 
-# Save the cropped image
-cv2.imwrite('output.jpg', cropped_image)
+    # Display the cropped image
+    cv2.imshow('Cropped and Gridded Image', cropped_image)
+    cv2.waitKey(1)
 
-# Close all windows
+    # Save the cropped image
+    cv2.imwrite('output.jpg', cropped_image)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):  # exit loop when 'q' key is pressed
+        break
+
 cv2.destroyAllWindows()
